000ad0a0df405e4128bf7391e8a6d3c8a6f243c6
Geraa50 <gera.a130000000000000@gmail.com>
2023-04-19 15:22:28 +0600

    URL to md, Bootstrap

---
+++ common/URL to md.py
@@ -0,0 +1,36 @@
+import requests
+import base64
+import sys
+
+# Введите ваш персональный токен доступа сюда
+TOKEN = "отправил в тг"
+
+
+def get_readme(owner, repo):
+    url = f"https://api.github.com/repos/{owner}/{repo}/readme"
+    headers = {"Authorization": f"token {TOKEN}"}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        content = base64.b64decode(response.json()["content"]).decode("utf-8")
+        return content
+    else:
+        print("Error")
+        sys.exit(1)
+
+
+def main():
+    github_url = input()
+    owner, repo = github_url.rstrip("/").split("/")[-2:]
+
+    readme = get_readme(owner, repo)
+
+    with open("Readme.md", "w", encoding="utf-8") as f:
+        f.write(readme)
+
+    print()
+
+
+if __name__ == "__main__":
+    main()
