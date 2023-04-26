5f91b46f2aadb6c240a4b42b2734c03f734e788e
Geraa <gera.a130000000000000@gmail.com>
2023-04-26 16:16:49 +0600

    change Url to md and topic on the centre

--- common/URL to md.py
+++ common/URL to md.py
@@ -1,36 +1,34 @@
 import requests
 import base64
-import sys
 
-# Введите ваш персональный токен доступа сюда
-TOKEN = "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"
+API_KEY = "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"
 
 
-def get_readme(owner, repo):
-    url = f"https://api.github.com/repos/{owner}/{repo}/readme"
-    headers = {"Authorization": f"token {TOKEN}"}
-
+def download_md_file(github_url):
+    owner, repo, md_filename = extract_github_info(github_url)
+    url = f"https://api.github.com/repos/{owner}/{repo}/contents/{md_filename}"
+    headers = {"Authorization": f"token {API_KEY}"}
     response = requests.get(url, headers=headers)
+    content = response.json()
 
-    if response.status_code == 200:
-        content = base64.b64decode(response.json()["content"]).decode("utf-8")
-        return content
-    else:
-        print("Error")
-        sys.exit(1)
+    if 'content' not in content:
+        print(f"Не удалось найти {md_filename} в репозитории.")
+        return
+
+    file_content = base64.b64decode(content['content']).decode('utf-8')
+    with open(md_filename, 'w', encoding='utf-8') as md_file:
+        md_file.write(file_content)
+    print(f"Файл {md_filename} успешно скачан.")
 
 
-def main():
-    github_url = input()
-    owner, repo = github_url.rstrip("/").split("/")[-2:]
-
-    readme = get_readme(owner, repo)
-
-    with open("Readme.md", "w", encoding="utf-8") as f:
-        f.write(readme)
-
-    print()
+def extract_github_info(github_url):
+    url_parts = github_url.strip('/').split('/')
+    owner = url_parts[-2]
+    repo = url_parts[-1]
+    md_filename = 'README.md'
+    return owner, repo, md_filename
 
 
 if __name__ == "__main__":
-    main()
+    github_url = input("Введите ссылку на GitHub проект: ")
+    download_md_file(github_url)
