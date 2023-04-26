9d50d27d2289c1deaf92dd5a1c7c4738940b32f6
dudava <Dudavik356@yandex.ru>
2023-04-26 16:46:18 +0600

    small changes

--- common/URL to md.py
+++ common/URL to md.py
@@ -1,7 +1,7 @@
 import requests
 import base64
 
-API_KEY = "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"
+API_KEY = "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"
 
 
 def download_md_file(github_url):
@@ -11,24 +11,13 @@ def download_md_file(github_url):
     response = requests.get(url, headers=headers)
     content = response.json()
 
-<<<<<<< HEAD
-    if response.status_code == 200:
-    # if True:
-        content = base64.b64decode(response.json()["content"]).decode("utf-8")
-        return content
-    else:
-        print("Error")
-        sys.exit(1)
-=======
     if 'content' not in content:
-        print(f"Не удалось найти {md_filename} в репозитории.")
+        print(f"Error Не удалось найти {md_filename} в репозитории.")
         return
-
     file_content = base64.b64decode(content['content']).decode('utf-8')
     with open(md_filename, 'w', encoding='utf-8') as md_file:
         md_file.write(file_content)
     print(f"Файл {md_filename} успешно скачан.")
->>>>>>> 5f91b46f2aadb6c240a4b42b2734c03f734e788e
 
 
 def extract_github_info(github_url):
